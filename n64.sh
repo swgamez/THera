6d8870a7edf3380d892ad6b86cc90f2a16a84d76
swgamez <wilgamez@hotmail.com>
2020-10-24 11:09:06 -0700

    Add files via upload

--- n64.sh
+++ n64.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 if  [[ $1 == "standalone" ]]; then
 /opt/mupen64plus/mupen64plus --resolution 320x240 --plugindir /opt/mupen64plus --gfx mupen64plus-video-rice.so --corelib /opt/mupen64plus/libmupen64plus.so.2 "$2"
